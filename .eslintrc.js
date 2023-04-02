ad8bc4a49319ea215b6bd6404287f86f6789cf66
qilekaca <1358390649@qq.com>
2023-04-02 14:31:09 +0800

    添加环境变量

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'vue/multiline-html-element-content-newline': 'off',
     'vue/name-property-casing': ['error', 'PascalCase'],
     'vue/no-v-html': 'off',
+    'vue/html-self-closing': 'off',
     'accessor-pairs': 2,
     'arrow-spacing': [
       2,
